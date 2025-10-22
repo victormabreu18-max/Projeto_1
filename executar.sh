cd4c8002fef79d5e933670bbd05294ba09d50b27
victormabreu18-max <victormabreu18@gmail.com>
2025-10-22 18:57:18 -0300

    Criado usando o Colab

---
+++ executar.sh
@@ -0,0 +1,38 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOaivpjvL1Qm3iy9wknEoAH"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "j5XHlfmj6xrL"
+      },
+      "outputs": [],
+      "source": [
+        "#!/bin/bash\n",
+        "\n",
+        "# Este script executa o código da calculadora.\n",
+        "echo \"Iniciando a Calculadora Python...\"\n",
+        "\n",
+        "# Executa o script Python\n",
+        "python3 calculadora.py\n",
+        "\n",
+        "echo \"Calculadora encerrada.\""
+      ]
+    }
+  ]
+}
